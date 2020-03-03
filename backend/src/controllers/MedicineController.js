2b06467423d6ee0a6bf889b1d481989d79483848
guilherme rodrigues <guilhermerodrigues123477@gmail.com>
2020-03-03 12:41:15 -0300

    adicao do socket.io na criacao de um objeto

--- backend/src/controllers/MedicineController.js
+++ backend/src/controllers/MedicineController.js
@@ -1,31 +1,31 @@
 const Medicine = require('../models/Medicine');
 const User = require('../models/User')
 module.exports = {
-  
+
   async index(req, res) {
 
     const medicines = await Medicine.find({});
-    
-    if(medicines) {
+
+    if (medicines) {
       return res.json(medicines);
     }
-  
+
   },
-  
+
   async store(req, res) {
 
     const { user_id } = req.headers;
-    
-    const { name, date, hour,description } = req.body;
+
+    const { name, date, hour, description } = req.body;
 
     let user = await User.findById(user_id);
-    
-    if (!user) {
-      return res.json(400).json({error: "User does not exist"});
-    } 
 
+    if (!user) {
+      return res.json(400).json({ error: "User does not exist" });
+    }
 
     let isDoctor = user.isDoctor;
+
     let recommendedByDoctor = isDoctor ? true : false;
 
     const medicine = await Medicine.create({
@@ -36,8 +36,12 @@ module.exports = {
       recommendedByDoctor,
       description
     });
+
     console.log(medicine);
-    return res.json(medicine).json({sucess: "Medicine create!"});
+
+    req.io.emit('medicine', medicine);
+
+    return res.json(medicine).json({ sucess: "Medicine create!" });
 
   },
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,31 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const routes = require('./routes');
+
 const app =  express();
+
+const server = require('http').Server(app);
+
+const io = require('socket.io')(server);
+
+
 mongoose.connect('mongodb+srv://guilherme:dev1212@cluster0-ptfgq.mongodb.net/medicine-reminder?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 )
+
+app.use((req,res,next) => {
+
+  req.io = io;
+
+  next()
+})
+
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333);
+
+server.listen(3333);  
