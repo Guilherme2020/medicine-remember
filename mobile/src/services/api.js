aa88fec5a8ffc28a7764dc0e1bbcc285d19c02b5
guilherme rodrigues <guilhermerodrigues123477@gmail.com>
2020-03-05 15:09:49 -0300

    changue url base for server

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const URL_BASE = "http://192.168.100.8:3333"//ip fixo da minha rede
-
+// const URL_BASE = "http://192.168.100.8:3333"//ip fixo da minha rede
+const URL_BASE = "https://medreminder-backend.herokuapp.com";
 const api = axios.create({
   baseURL: URL_BASE,
 });
