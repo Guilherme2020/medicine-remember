99c59ceb4aa3e4b778e965dcbf9b18f0fee798eb
guilherme rodrigues <guilhermerodrigues123477@gmail.com>
2020-03-05 15:01:21 -0300

    config variavel de ambiente para deploy

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const api = axios.create({
   //https://medreminder-backend.herokuapp.com servidor 
   // baseURL: 'http://localhost:3333',//meu localhost
-  baseURL: 'https://medreminder-backend.herokuapp.com'
+  // baseURL: 'https://medreminder-backend.herokuapp.com'
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 
